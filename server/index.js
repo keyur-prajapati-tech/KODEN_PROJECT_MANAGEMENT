bea4dbb64ccc0020478f3f13c7fcdd7bfc4d5be4
Kevin <kevinpatel@coder.com>
2026-03-04 15:47:38 +0530

    change in index.js

--- server/index.js
+++ server/index.js
@@ -35,7 +35,15 @@ app.use(cors());
 app.use(clerkMiddleware())
 
 // Set up the "/api/inngest" (recommended) routes with the serve handler
-app.use("/api/inngest", serve({ client: inngest, functions }));
+// app.use("/api/inngest", serve({ client: inngest, functions }));
+app.use(
+  "/api/inngest", 
+  serve({ 
+    client: inngest, 
+    functions,
+    signingKey: process.env.INNGEST_SIGNING_KEY
+  })
+);
 
 app.get("/",(req,res)=>{
   res.send("Welcome To Project Management System");
